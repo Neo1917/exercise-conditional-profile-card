819eddb7435122b6065301b87965175348295428
Neo1917 <jhongomez191219@gmail.com>
2025-11-12 05:17:42 +0000

    Conditional-profile-Card-Jhon

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,17 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? "Jhon" : variables.name} ${
+    variables.lastName == null ? "Gomez" : variables.lastName
+  }</h1>
+          <h2>${variables.role == null ? "Programmer" : variables.role}</h2>
+          <h3>${variables.city == null ? "Bogota" : variables.city},
+          ${variables.country == null ? "Colombia" : variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://instagram.com/@riosjhon839"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
